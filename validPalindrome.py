2065692632a5c8f3ee9689b277c1deac761c1f96
Yu Heng Lee <lee2000yh@gmail.com>
2022-07-13 21:14:54 +0800

    alternative solution for validPalindrome

--- validPalindrome.py
+++ validPalindrome.py
@@ -1,3 +1,6 @@
+from numpy import character
+
+
 def isPalindrome(s: str) -> bool:
   lst = []
   for char in s:
@@ -8,5 +11,26 @@ def isPalindrome(s: str) -> bool:
   
   temp = lst[::-1]
   return temp == lst
+
+def isPalindrome2(s: str) -> bool:
+  l = 0
+  r = len(s) - 1
+  while l < r:
+    while l < r and not alphaNum(s[l]):
+      l += 1
+    while r > 1 and not alphaNum(s[r]):
+      r -= 1
+
+    if s[l].lower() != s[r].lower():
+      return False
+    
+    l += 1
+    r -= 1
+  return True
+
+def alphaNum(c: character) -> bool:
+  return (ord('A') <= ord(c) <= ord('Z') or 
+      ord('a') <= ord(c) <= ord('z') or 
+      ord('0') <= ord(c) <= ord('9'))
 print(isPalindrome("A man, a plan, a canal: Panama"))
 print(isPalindrome(".,"))
