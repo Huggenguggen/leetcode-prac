b489dacfaeeec180cf72268f7d549a5a5ce2f90c
Yu Heng Lee <lee2000yh@gmail.com>
2022-07-14 21:46:44 +0800

    Update longestSubstring.py

--- longestSubstring.py
+++ longestSubstring.py
@@ -14,6 +14,11 @@ test_case_1 = "abcabcbb"
 test_case_2 = "bbbbb"
 test_case_3 = "pwwkew"
 
-print(lengthOfLongestSubstring(test_case_1))
+def test_func():
+  assert lengthOfLongestSubstring(test_case_1) == 3, "test case: 'abcabcbb' Should be 3"
+  assert lengthOfLongestSubstring(test_case_2) == 1, "test case: 'bbbbb' Should be 1"
+  assert lengthOfLongestSubstring(test_case_3) == 3, "test case: 'pwwkew' Should be 3"
 
-assert lengthOfLongestSubstring(test_case_1) == 3, "test case: 'abcabcbb' Should be 3"
+if __name__ == "__main__":
+  test_func()
+  print("Everything passed")
