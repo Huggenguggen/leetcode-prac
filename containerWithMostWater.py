95e406fa02a931956972d1a4427584b92dbd61e0
Huggenguggen <lee2000yh@gmail.com>
2022-07-14 10:14:08 +0800

    Create containerWithMostWater.py

---
+++ containerWithMostWater.py
@@ -0,0 +1,19 @@
+from typing import List
+
+def maxArea(height: List[int]) -> int:
+  res = 0
+  l, r = 0, len(height) - 1
+
+  while l < r:
+    area = min(height[l], height[r]) * (r - l)
+    res = max(area, res)
+    if height[l] < height[r]:
+      l += 1
+    else:
+      r -= 1
+
+  return res
+
+
+
+print(maxArea([1, 8, 6, 2, 5,4, 8, 3, 7]))
