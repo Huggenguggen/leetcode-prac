d0822e4e88d00c8e66c89190efbd3dd14549a464
Yu Heng Lee <lee2000yh@gmail.com>
2022-07-20 21:37:30 +0800

    Update kokoEatingBananas.py

--- kokoEatingBananas.py
+++ kokoEatingBananas.py
@@ -1,5 +1,5 @@
 from typing import List
-import math
+import math, time
 
 def minEatingSpeed(piles: List[int], h: int) -> int:
   lo, hi = 1, max(piles)
@@ -19,7 +19,13 @@ def minEatingSpeed(piles: List[int], h: int) -> int:
   
   return min_rate
 
-
+def minEatingSpeed2(piles: List[int], h: int) -> int:
+    l, r = 1, max(piles)
+    while l < r:
+        mid = (l + r) // 2
+        if h < sum(math.ceil(a / mid) for a in piles): l = mid + 1
+        else: r = mid
+    return l
 
 
 
@@ -27,9 +33,19 @@ def minEatingSpeed(piles: List[int], h: int) -> int:
 
 
 def test_func():
+  st = time.time_ns()
   assert minEatingSpeed([3,6,7,11], 8) == 4
   assert minEatingSpeed([30,11,23,4,20], 5) == 30
   assert minEatingSpeed([30,11,23,4,20], 6) == 23
+  et = time.time_ns()
+  print('execution time of first method: ', et - st)
+
+  assert minEatingSpeed2([3,6,7,11], 8) == 4
+  assert minEatingSpeed2([30,11,23,4,20], 5) == 30
+  assert minEatingSpeed2([30,11,23,4,20], 6) == 23
+  e2t = time.time_ns()
+  print('execution time of second method: ', e2t - et)
+
 
 if __name__ == "__main__":
   test_func()
