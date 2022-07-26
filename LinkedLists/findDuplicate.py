dc98a38dbaa3d3d382a33537406a5b55143ad871
Yu Heng Lee <lee2000yh@gmail.com>
2022-07-26 11:11:48 +0800

    Create findDuplicate.py

---
+++ LinkedLists/findDuplicate.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from ListNode import ListNode
+
+#Floyd's algo
+def findDuplicate(nums: List[int]) -> int:
+  slow, fast = 0, 0
+
+  while True:
+    slow = nums[slow]
+    fast = nums[nums[fast]]
+
+    if slow == fast:
+      break
+
+  slow2 = 0
+  while True:
+    slow = nums[slow]
+    slow2 = nums[slow2]
+    if slow == slow2:
+      return slow
